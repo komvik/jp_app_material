798f9b116808c513cf298845577ff1f81a7e2f16
Viktor <komvik.slack@gmail.com>
2024-10-16 23:49:33 +0200

    clean main

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:jp_app_material/screen1/screens/start_screen_app.dart';
-// import 'package:jp_app_material/screen2/screens/main_screen_app.dart';
-// import 'package:jp_app_material/screen3/detailed_info_screen.dart';
 
 void main() {
   runApp(const StartScreenApp());
-//  runApp(const MainScreenApp());
-//  runApp(const DetailedInfoScreen());
 }
